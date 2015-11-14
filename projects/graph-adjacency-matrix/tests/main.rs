b342e219118160c945798f4b2f113afb839e52a9
Aster <galaster@foxmail.com>
2015-11-14 00:55:23 +0000

    📦 Add the graph theory project

--- projects/graph-adjacency-matrix/tests/main.rs
+++ projects/graph-adjacency-matrix/tests/main.rs
@@ -12,12 +12,12 @@ fn test() {
     let mut graph = StaticUndirected::new(5);
     graph.connect((1, 3)).unwrap();
     graph.connect((1, 4)).unwrap();
-    println!("{}", graph.to_wolfram_string());
+    println!("{}", graph);
     println!("undirected graph:");
     let mut graph = StaticDirected::new(5);
     graph.connect((2, 3)).unwrap();
     graph.connect((2, 4)).unwrap();
     graph.connect((3, 1)).unwrap();
     graph.connect((4, 1)).unwrap();
-    println!("{}", graph.to_wolfram_string());
+    println!("{}", graph);
 }
